bada3cb5f5a9d8eb2c92cc81f0e485ceaa469863
Peachcoding97 <187660744+Peachboy97@users.noreply.github.com.>
2025-11-21 15:53:44 +0000

    Use conditional if statements to randomonly produce the computer's choice. Basically set the conditions so that 0, 1, 2 will be assigned to either rock,paper, or scissors

--- javascript.js
+++ javascript.js
@@ -1,7 +1,13 @@
 // console.log("Hello world!");
 
 function getComputerChoice () {
-    return Math.floor(Math.random()*3); 
+    if (Math.floor(Math.random()*3) == 0) {
+        return "Rock!";
+    } else if (Math.floor(Math.random()*3) == 1) {
+        return "Paper!";
+    } else {
+        return "Scissors!";
+    }
 }
 
 
